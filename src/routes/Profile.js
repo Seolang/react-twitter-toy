4f8cfa6b5ea0edb90675fdeba327f60f7e42e71a
Seolang <dntmdqls0912@gmail.com>
2022-10-06 10:53:39 +0900

    profile logout sys using useNavigate() and auth.signOut()

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,19 @@
+import { authService } from "fBase";
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useNavigate } from "react-router-dom"; // useHistory deprecated and then useNavigate used
 
 const Profile = () => {
-  return <span>Profile</span>;
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    signOut(authService);
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default Profile;
